dae2cfd1db5065f6b06b616958546e62f8488066
J M pramod <pramod@ctinfopro.com>
2024-06-14 16:22:49 +0530

    UPDATING LATEST ROUTE

--- app/product/Product.tsx
+++ app/product/Product.tsx
@@ -19,7 +19,7 @@ const Product = (props: any) => {
       console.log('Fetching products...');
 
       const response = await axios.get(
-        `http://localhost:3000/api/register?Side_Stone=${sideStone}&Metal_Type=${metalType}&Center_Stone=${centerStone}`
+        `https://next-poc-1.netlify.app/register?Side_Stone=${sideStone}&Metal_Type=${metalType}&Center_Stone=${centerStone}`
       );
       router.push(
         `product/?Side_Stone=${sideStone}&Metal_Type=${metalType}&Center_Stone=${centerStone}`
