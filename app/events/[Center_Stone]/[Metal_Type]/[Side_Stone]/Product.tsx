0cf450c75f1babd0612ed35195ce0da9c41c87fd
J M pramod <pramod@ctinfopro.com>
2024-06-17 21:39:33 +0530

    added skeliton

--- app/events/[Center_Stone]/[Metal_Type]/[Side_Stone]/Product.tsx
+++ app/events/[Center_Stone]/[Metal_Type]/[Side_Stone]/Product.tsx
@@ -29,7 +29,7 @@ const Product = (props: any) => {
   // Fetch product list whenever state changes
   useEffect(() => {
     const fetchProduct = async () => {
-      setLoading(true);
+      // setLoading(true);
       const response = await axios.get(
         `https://next-poc-1.netlify.app/api/events/${centerStone}/${metalType}/${sideStone}`
       );
@@ -87,123 +87,194 @@ const Product = (props: any) => {
 
   return (
     <>
-      {/* {loading || true ? (
-        <div style={{ display: 'flex', width: '100%', height: '100vh' }}>
-          <div>
-          <styles.skeletonContainer/>
+      {loading || true ? (
+        <styles.skelitonWrapper>
+          <styles.skeletonContainer>
+            <styles.skeletonLoader />
+          </styles.skeletonContainer>
+          <styles.skelitonInnerWrapper>
+            <styles.skelitonRectangle
+              variant="rectangular"
+              height={60}
+            ></styles.skelitonRectangle>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '3px',
+                width: '40%',
+              }}
+            >
+              <styles.skelitonRectangle1
+                variant="rectangular"
+                height={30}
+              ></styles.skelitonRectangle1>
+              <styles.skelitonRectangle2
+                variant="rectangular"
+                height={30}
+              ></styles.skelitonRectangle2>
+            </div>
 
-          </div>
-        </div>
-      ) : ( */}
-      <styles.outerContainer>
-        {/* image slider */}
-        <styles.sliderContainer>
-          <Slider selectedJewelry={productList} />
-        </styles.sliderContainer>
-        {/* Information slider */}
-        <styles.infoContainer>
-          <styles.title variant="h4">
-            {productList?.description
-              ? productList?.title
-              : 'Sorry, no products matched your selection. Please choose a different combination.'}
-          </styles.title>
-          <styles.startingAt variant="h5">
-            Starting at: $489.00
-          </styles.startingAt>
-          <styles.titleContainer>
-            <Typography variant="body1">Center Stone :</Typography>
-            <Typography>{centerStone}</Typography>
-          </styles.titleContainer>
-          <styles.titleContainer>
-            <styles.IconImage
-              src="https://demo.elbasoft.com/Nextjs_POC/images/Variations-Stone-Blue-Sapphire-A-45x45.png"
-              alt="Blue Sapphire"
-              onClick={() => handleCenterStoneChange('Blue Sapphire')}
-              style={{ border: getBorderStyle(centerStone, 'Blue Sapphire') }}
-            />
-            <styles.IconImage
-              src="https://demo.elbasoft.com/Nextjs_POC/images/Variations-Stone-Green-Emerald-A-45x45.png"
-              alt="Green Emerald"
-              onClick={() => handleCenterStoneChange('Emerald')}
-              style={{ border: getBorderStyle(centerStone, 'Emerald') }}
-            />
-            <styles.IconImage
-              src="https://demo.elbasoft.com/Nextjs_POC/images/Morganite-gemstone-45x45.png"
-              alt="Morganite"
-              onClick={() => handleCenterStoneChange('Morganite')}
-              style={{ border: getBorderStyle(centerStone, 'Morganite') }}
-            />
-            <styles.IconImage
-              src="https://demo.elbasoft.com/Nextjs_POC/images/pink-sapphaire-45x45.png"
-              alt="Pink Sapphire"
-              onClick={() => handleCenterStoneChange('Pink Sapphire')}
-              style={{ border: getBorderStyle(centerStone, 'Pink Sapphire') }}
-            />
-            <styles.IconImage
-              src="https://demo.elbasoft.com/Nextjs_POC/images/Variations-Stone-Red-Ruby-A-45x45.png"
-              alt="Red Ruby"
-              onClick={() => handleCenterStoneChange('Ruby')}
-              style={{ border: getBorderStyle(centerStone, 'Ruby') }}
-            />
-            <styles.IconImage
-              src="https://demo.elbasoft.com/Nextjs_POC/images/tanzanite-45x45.png"
-              alt="Tanzanite"
-              onClick={() => handleCenterStoneChange('Tanzanite')}
-              style={{ border: getBorderStyle(centerStone, 'Tanzanite') }}
-            />
-          </styles.titleContainer>
-          <styles.titleContainer>
-            <Typography variant="body1">Metal Type:</Typography>
-            <Typography>{metalType}</Typography>
-          </styles.titleContainer>
-          <styles.titleContainer>
-            <styles.IconImage
-              src="https://demo.elbasoft.com/Nextjs_POC/images/3-2-45x45.png"
-              alt="Rose Gold"
-              onClick={() => handleMetalTypeChange('Rose Gold')}
-              style={{ border: getBorderStyle(metalType, 'Rose Gold') }}
-            />
-            <styles.IconImage
-              src="https://demo.elbasoft.com/Nextjs_POC/images/1-2-45x45.png"
-              alt="White Gold"
-              onClick={() => handleMetalTypeChange('White Gold')}
-              style={{ border: getBorderStyle(metalType, 'White Gold') }}
-            />
-            <styles.IconImage
-              src="https://demo.elbasoft.com/Nextjs_POC/images/2-2-45x45.png"
-              alt="Yellow Gold"
-              onClick={() => handleMetalTypeChange('Yellow Gold')}
-              style={{ border: getBorderStyle(metalType, 'Yellow Gold') }}
-            />
-          </styles.titleContainer>
-          <styles.titleContainer>
-            <Typography variant="body1">Side Stone:</Typography>
-            <Typography>{sideStone}</Typography>
-          </styles.titleContainer>
-          <styles.titleContainer>
-            <styles.IconImage
-              src="https://demo.elbasoft.com/Nextjs_POC/images/About1-45x45.png"
-              alt="Lab Diamond"
-              onClick={() => handleSideStoneChange('Lab Diamond')}
-              style={{ border: getBorderStyle(sideStone, 'Lab Diamond') }}
-            />
-            <styles.IconImage
-              src="https://demo.elbasoft.com/Nextjs_POC/images/About-45x45.png"
-              alt="Natural Diamond"
-              onClick={() => handleSideStoneChange('White Diamond')}
-              style={{ border: getBorderStyle(sideStone, 'White Diamond') }}
-            />
-          </styles.titleContainer>
-          <ul style={{ paddingLeft: '10px' }}>
-            {formatDescription(
-              productList?.description
-                ? productList?.description
-                : 'Description not available for product'
-            )}
-          </ul>
-        </styles.infoContainer>
-      </styles.outerContainer>
-      {/* )} */}
+            <styles.skelitonRectangle2
+              variant="rectangular"
+              height={20}
+              width={'30%'}
+            ></styles.skelitonRectangle2>
+            <div
+              style={{
+                display: 'flex',
+                gap: '15px',
+              }}
+            >
+              <Skeleton variant="circular" width={60} height={60} />
+              <Skeleton variant="circular" width={60} height={60} />
+              <Skeleton variant="circular" width={60} height={60} />
+              <Skeleton variant="circular" width={60} height={60} />
+              <Skeleton variant="circular" width={60} height={60} />
+              <Skeleton variant="circular" width={60} height={60} />
+            </div>
+
+            <styles.skelitonRectangle2
+              variant="rectangular"
+              height={20}
+              width={'30%'}
+            ></styles.skelitonRectangle2>
+            <div
+              style={{
+                display: 'flex',
+                gap: '15px',
+              }}
+            >
+              <Skeleton variant="circular" width={60} height={60} />
+              <Skeleton variant="circular" width={60} height={60} />
+              <Skeleton variant="circular" width={60} height={60} />
+            </div>
+            <styles.skelitonRectangle2
+              variant="rectangular"
+              height={20}
+              width={'30%'}
+            ></styles.skelitonRectangle2>
+            <div
+              style={{
+                display: 'flex',
+                gap: '15px',
+              }}
+            >
+              <Skeleton variant="circular" width={60} height={60} />
+              <Skeleton variant="circular" width={60} height={60} />
+            </div>
+          </styles.skelitonInnerWrapper>
+        </styles.skelitonWrapper>
+      ) : (
+        <styles.outerContainer>
+          {/* image slider */}
+          <styles.sliderContainer>
+            <Slider selectedJewelry={productList} />
+          </styles.sliderContainer>
+          {/* Information slider */}
+          <styles.infoContainer>
+            <styles.title variant="h4">
+              {productList?.description
+                ? productList?.title
+                : 'Sorry, no products matched your selection. Please choose a different combination.'}
+            </styles.title>
+            <styles.startingAt variant="h5">
+              Starting at: $489.00
+            </styles.startingAt>
+            <styles.titleContainer>
+              <Typography variant="body1">Center Stone :</Typography>
+              <Typography>{centerStone}</Typography>
+            </styles.titleContainer>
+            <styles.titleContainer>
+              <styles.IconImage
+                src="https://demo.elbasoft.com/Nextjs_POC/images/Variations-Stone-Blue-Sapphire-A-45x45.png"
+                alt="Blue Sapphire"
+                onClick={() => handleCenterStoneChange('Blue Sapphire')}
+                style={{ border: getBorderStyle(centerStone, 'Blue Sapphire') }}
+              />
+              <styles.IconImage
+                src="https://demo.elbasoft.com/Nextjs_POC/images/Variations-Stone-Green-Emerald-A-45x45.png"
+                alt="Green Emerald"
+                onClick={() => handleCenterStoneChange('Emerald')}
+                style={{ border: getBorderStyle(centerStone, 'Emerald') }}
+              />
+              <styles.IconImage
+                src="https://demo.elbasoft.com/Nextjs_POC/images/Morganite-gemstone-45x45.png"
+                alt="Morganite"
+                onClick={() => handleCenterStoneChange('Morganite')}
+                style={{ border: getBorderStyle(centerStone, 'Morganite') }}
+              />
+              <styles.IconImage
+                src="https://demo.elbasoft.com/Nextjs_POC/images/pink-sapphaire-45x45.png"
+                alt="Pink Sapphire"
+                onClick={() => handleCenterStoneChange('Pink Sapphire')}
+                style={{ border: getBorderStyle(centerStone, 'Pink Sapphire') }}
+              />
+              <styles.IconImage
+                src="https://demo.elbasoft.com/Nextjs_POC/images/Variations-Stone-Red-Ruby-A-45x45.png"
+                alt="Red Ruby"
+                onClick={() => handleCenterStoneChange('Ruby')}
+                style={{ border: getBorderStyle(centerStone, 'Ruby') }}
+              />
+              <styles.IconImage
+                src="https://demo.elbasoft.com/Nextjs_POC/images/tanzanite-45x45.png"
+                alt="Tanzanite"
+                onClick={() => handleCenterStoneChange('Tanzanite')}
+                style={{ border: getBorderStyle(centerStone, 'Tanzanite') }}
+              />
+            </styles.titleContainer>
+            <styles.titleContainer>
+              <Typography variant="body1">Metal Type:</Typography>
+              <Typography>{metalType}</Typography>
+            </styles.titleContainer>
+            <styles.titleContainer>
+              <styles.IconImage
+                src="https://demo.elbasoft.com/Nextjs_POC/images/3-2-45x45.png"
+                alt="Rose Gold"
+                onClick={() => handleMetalTypeChange('Rose Gold')}
+                style={{ border: getBorderStyle(metalType, 'Rose Gold') }}
+              />
+              <styles.IconImage
+                src="https://demo.elbasoft.com/Nextjs_POC/images/1-2-45x45.png"
+                alt="White Gold"
+                onClick={() => handleMetalTypeChange('White Gold')}
+                style={{ border: getBorderStyle(metalType, 'White Gold') }}
+              />
+              <styles.IconImage
+                src="https://demo.elbasoft.com/Nextjs_POC/images/2-2-45x45.png"
+                alt="Yellow Gold"
+                onClick={() => handleMetalTypeChange('Yellow Gold')}
+                style={{ border: getBorderStyle(metalType, 'Yellow Gold') }}
+              />
+            </styles.titleContainer>
+            <styles.titleContainer>
+              <Typography variant="body1">Side Stone:</Typography>
+              <Typography>{sideStone}</Typography>
+            </styles.titleContainer>
+            <styles.titleContainer>
+              <styles.IconImage
+                src="https://demo.elbasoft.com/Nextjs_POC/images/About1-45x45.png"
+                alt="Lab Diamond"
+                onClick={() => handleSideStoneChange('Lab Diamond')}
+                style={{ border: getBorderStyle(sideStone, 'Lab Diamond') }}
+              />
+              <styles.IconImage
+                src="https://demo.elbasoft.com/Nextjs_POC/images/About-45x45.png"
+                alt="Natural Diamond"
+                onClick={() => handleSideStoneChange('White Diamond')}
+                style={{ border: getBorderStyle(sideStone, 'White Diamond') }}
+              />
+            </styles.titleContainer>
+            <ul style={{ paddingLeft: '10px' }}>
+              {formatDescription(
+                productList?.description
+                  ? productList?.description
+                  : 'Description not available for product'
+              )}
+            </ul>
+          </styles.infoContainer>
+        </styles.outerContainer>
+      )}
     </>
   );
 };
--- app/events/[Center_Stone]/[Metal_Type]/[Side_Stone]/styles.ts
+++ app/events/[Center_Stone]/[Metal_Type]/[Side_Stone]/styles.ts
@@ -38,9 +38,48 @@ export const styles = {
     border-radius: 100%;
     cursor: pointer;
   `,
-  skeletonContainer:styled(Skeleton)`
-  width:50%;
-  height:80vh;
-  
-  `
+  skelitonWrapper: styled('div')`
+    display: flex;
+    width: 100%;
+    @media (0<width<700px) {
+      flex-direction: column;
+    }
+  `,
+  skeletonContainer: styled('div')`
+    display: flex;
+    width: 50%;
+    height: 100vh;
+    @media (0<width<700px) {
+      height: 40vh;
+      width: 100%;
+    }
+  `,
+  skelitonInnerWrapper: styled('div')`
+    width: 50%;
+    margin: 12px;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    @media (0<width<700px) {
+      width: 93%;
+
+      margin: 0 12px;
+    }
+  `,
+
+  skeletonLoader: styled(Skeleton)`
+    width: 100%;
+    height: 80%;
+    transform: scale(1) !important;
+    margin: 12px;
+  `,
+  skelitonRectangle: styled(Skeleton)`
+    width: 100%;
+  `,
+  skelitonRectangle1: styled(Skeleton)`
+    width: 100%;
+  `,
+  skelitonRectangle2: styled(Skeleton)`
+    width: 100%;
+  `,
 };
